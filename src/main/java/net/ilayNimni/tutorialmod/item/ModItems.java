61cb9c2d49fa473e53421652344dd411ea810eee
ilayNimni <ilaynimni12345@gmail.com>
2023-07-23 18:34:20 +0300

    ani

--- src/main/java/net/ilayNimni/tutorialmod/item/ModItems.java
+++ src/main/java/net/ilayNimni/tutorialmod/item/ModItems.java
@@ -3,8 +3,9 @@ package net.ilayNimni.tutorialmod.item;
 import net.ilayNimni.tutorialmod.TutorialMod;
 import net.ilayNimni.tutorialmod.block.ModBlocks;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemNameBlockItem;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -26,6 +27,32 @@ public class ModItems {
     public static final RegistryObject<Item> BLUEBERRY = ITEMS.register("blueberry",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)
                     .food(new FoodProperties.Builder().nutrition(2).saturationMod(2f).build())));
+
+    public static final RegistryObject<SwordItem> ZIRCON_SWORD = ITEMS.register("zircon_sword",
+            () -> new SwordItem(null, 5, 3.5f, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<ShovelItem> ZIRCON_SHOVEL = ITEMS.register("zircon_shovel",
+            () -> new ShovelItem(null, 0, 1f, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<PickaxeItem> ZIRCON_PICKAXE = ITEMS.register("zircon_pickaxe",
+            () -> new PickaxeItem(null, 2, 2.5f, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<AxeItem> ZIRCON_AXE = ITEMS.register("zircon_axe",
+            () -> new AxeItem(null, 3, 3.5f, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<HoeItem> ZIRCON_HOE = ITEMS.register("zircon_hoe",
+            () -> new HoeItem(null, 0, 0, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static class Tiers {
+        public static final Tier ZIRCON = new ForgeTier(
+                3,
+                800,
+                1.5f,
+                3,
+                350,
+                null,
+                () -> Ingredient.of(ModItems.ZIRCON.get()));
+    }
+
 
 
     public static void register(IEventBus eventBus) {
